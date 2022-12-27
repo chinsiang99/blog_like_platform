15b6207618b1b04795934576ec7d291ae7339c51
chinsiang99 <chinsiang.tan@xeersoft.com>
2022-12-27 14:02:04 +0800

    feat(blogpost): pagination for blog posts

--- controller/homeController.js
+++ controller/homeController.js
@@ -9,22 +9,69 @@ const getHomePage = asyncHandler(async (req,res,next)=>{
 
   const postCollection = collection(db, "blogpost");
   const docsSnap = await getDocs(postCollection);
+  const { offset } = req.query;
   // console.log(typeof docsSnap);
   // console.log(docsSnap.docs.length);
   const total_length = docsSnap.docs.length;
-  const result = await query(postCollection,
-    limit(25), offset(1));
+  let offset_value = offset ?? 0;
+  let counter = 0;
+
+  let next_offset = "";
+  let prev_offset = "";
+  if(offset_value == total_length-1){
+    next_offset = -1;
+    prev_offset = +offset_value -1;
+    
+  }else if(offset_value < total_length-1){
+    next_offset = +offset_value + 1;
+    prev_offset = +offset_value - 1;
+  }
+
+  if(total_length == 1 && offset_value == 0){
+    prev_offset = -1;
+    next_offset = -1;
+  }
+
+  let data = {
+    id: "",
+    title: "",
+    description: "",
+    user_email: "",
+    next_offset: next_offset,
+    prev_offset: prev_offset,
+    total_length: total_length
+  };  
+
+  // console.log(docsSnap);
+
+  if(offset_value > total_length){
+    throw new Error("There are no further more records to be shown!");
+  }
+
+  docsSnap.forEach((doc) => {
+    if(counter == offset_value){
+      data.id = doc.id;
+      data.title = doc.data().title;
+      data.description = doc.data().description;
+      data.user_email = doc.data().user_email;
+      console.log(data);
+    }
+    counter++;
+  })
+
+  // const result = await query(postCollection,
+  //   limit(25), offset(1));
   // console.log(typeof result);
   // console.log(result.docs.length);
 
-  onSnapshot(result, (snapshot)=>{
-    if(snapshot.docs.length){
+  // onSnapshot(result, (snapshot)=>{
+  //   if(snapshot.docs.length){
 
-    }else{
-      console.log("gg");
-    }
-    console.log(snapshot.docs.length);
-  })
+  //   }else{
+  //     console.log("gg");
+  //   }
+  //   console.log(snapshot.docs.length);
+  // })
   
   // result.forEach(doc => {
   //   console.log(doc.id);
@@ -47,7 +94,7 @@ const getHomePage = asyncHandler(async (req,res,next)=>{
   // console.log(q.empty);
 
 
-  res.render("home", {status: 200});
+  res.render("home", data);
 });
 
 module.exports = { getHomePage }
