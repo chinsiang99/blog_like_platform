633cf78d887a6e56c8eaacc8e4467c18f336df85
chinsiang99 <chinsiang.tan@xeersoft.com>
2022-12-30 21:31:07 +0800

    adding like and comment route in index.js

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,17 @@ const registerRoutes = require("./registerRoutes");
 const loginRoutes = require("./loginRoutes");
 const homeRoutes = require("./homeRoutes");
 const blogRoutes = require("./blogRoutes");
+const commentRoutes = require("./commentRoutes");
+const likeRoutes = require("./likeRoutes");
 
 const router = Router();
 
+// registering routes here
 router.use("/register/", registerRoutes);
 router.use("/login/", loginRoutes);
 router.use("/home/", homeRoutes);
 router.use("/blog/", blogRoutes);
+router.use("/comment/", commentRoutes);
+router.use("/like/", likeRoutes);
 
 module.exports = router;
