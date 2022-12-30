de05275c64db282306b2391f05e098bdb1a7080d
chinsiang99 <chinsiang.tan@xeersoft.com>
2022-12-30 21:32:01 +0800

    remove unwanted files and comments

--- routes/registerRoutes.js
+++ routes/registerRoutes.js
@@ -1,17 +1,7 @@
 const { Router } = require("express");
 const { getRegisterPage, register } = require("../controller/registerController");
-// const validateToken = require("../middleware/validateTokenHandler");
-// const {createContact, getAllContacts, getSpecificContact, updateContact, deleteContact} = require("../controllers/contactController");
-// const validateUserExists = require("../middleware/validateUserExist");
 
 const router = Router();
-
-// define routes after the "/contacts/" prefix
-// api/contacts/
-
-// since all the endpoint inside contact route should be private, so every endpoint will have to gone through validate token middleware and validate whether the user exists
-// router.use(validateToken);
-// router.use(validateUserExists);
 
 // route to get register page
 router.route("/").get(
